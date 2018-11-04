4e2303526bb30a73bf72845223a544dcc2baeb60
Octávio Richter <octaviorichter@gmail.com>
2018-11-04 21:06:09 -0200

    Disable lock files & configure font by customize

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,9 +1,9 @@
 ;; https://github.com/purcell/emacs.d/blob/master/README.md#changing-themes-and-adding-your-own-customization
 
-(set-default-font "Monaco 16")
-
 (global-linum-mode t)
 
 (setq-default tab-width 4)
+
+(setq create-lockfiles nil)
 
 (provide 'init-local)
