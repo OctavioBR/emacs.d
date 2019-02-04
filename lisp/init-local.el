963a4f02c2ecb44acaa3cdbd1ede675603bdecca
Octávio Richter <octaviorichter@gmail.com>
2019-02-04 21:39:25 -0200

    Configure goimports & default frame width + height

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -6,4 +6,17 @@
 
 (setq create-lockfiles nil)
 
+;; (require 'editorconfig)
+;; (editorconfig-mode 1)
+
+;; Applies goimports (if executabvle available, otherwise fmt) before saving go code
+(with-eval-after-load 'go-mode
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (let ((goimports (executable-find "goimports")))
+    (when goimports
+      (setq gofmt-command goimports))))
+
+(add-to-list 'default-frame-alist '(width  . 125))
+(add-to-list 'default-frame-alist '(height . 43))
+
 (provide 'init-local)
