f7cbbafef7a2edcd0240b5a88134ba643c744196
Octávio Richter <octaviorichter@gmail.com>
2019-06-06 17:45:02 -0300

    Configure Malpa & add dracula theme

--- init.el
+++ init.el
@@ -1,5 +1,24 @@
+; Use "M-x eval-buffer" to avoid reopening emacs every time
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 
+; Editor UI
 (global-linum-mode t)      ; show line numbers
 (tool-bar-mode -1)         ; remove tool bar
 (menu-bar-mode -1)         ; remove menu bar
@@ -9,7 +28,7 @@
 (add-to-list 'default-frame-alist '(width  . 125))
 (add-to-list 'default-frame-alist '(height . 43))
 
-; don't save backup files next to original, put them in /tmp
+; Don't save backup files next to original, put them in /tmp
 (setq backup-directory-alist
 	  `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
@@ -35,5 +54,19 @@
 (global-set-key [(control shift up)]  'move-line-up)
 (global-set-key [(control shift down)]  'move-line-down)
 
-;; Dracula Theme
-;; TODO
+; Dracula Theme
+(load-theme 'dracula t)
+
+; Customize
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (dracula-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
