4d9eb0b47cd9b27e8f1800722b11a82705b92b4f
Octávio Richter <octaviorichter@gmail.com>
2019-06-06 18:16:25 -0300

    Simplify Melpa setup code

--- init.el
+++ init.el
@@ -1,41 +1,31 @@
-; Use "M-x eval-buffer" to avoid reopening emacs every time
+;; [TIP] Use "M-x eval-buffer" to avoid reopening emacs every time
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+
+;; Configure Melpa
+(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/") t)
+;; To add stable channel
+;; (add-to-list 'package-archives (cons "melpa-stable" "https://stable.melpa.org/packages/") t)
+
 (package-initialize)
 
-; Editor UI
+;; Editor UI
 (global-linum-mode t)      ; show line numbers
 (tool-bar-mode -1)         ; remove tool bar
 (menu-bar-mode -1)         ; remove menu bar
 (setq-default tab-width 4) ; tab with 4 spaces
 
-; Default window size
+;; Default window size
 (add-to-list 'default-frame-alist '(width  . 125))
 (add-to-list 'default-frame-alist '(height . 43))
 
-; Don't save backup files next to original, put them in /tmp
+;; Don't save backup files next to original, put them in /tmp
 (setq backup-directory-alist
 	  `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
 	  `((".*" ,temporary-file-directory t)))
 (setq create-lockfiles nil)
 
-; Functions to move line up and down
+;; Functions to move line up and down
 (defun move-line-up ()
   "Move up the current line."
   (interactive)
@@ -54,16 +44,18 @@ There are two things you can do about this warning:
 (global-set-key [(control shift up)]  'move-line-up)
 (global-set-key [(control shift down)]  'move-line-down)
 
-; Dracula Theme
+;; Dracula Theme
 (load-theme 'dracula t)
 
-; Hack Font
+;; Hack Font
 (set-face-attribute 'default t :font "Hack 14")
 
-; Open buffer menu in same window (keybind)
+;; Open buffer menu in same window (keybind)
 (global-set-key (kbd "C-x C-b") 'buffer-menu)
 
-; Customize
+;; Go development
+
+;; Customize
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
