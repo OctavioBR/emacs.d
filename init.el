f9013c51af92dc442e319e62a81e47a400729848
Octávio Richter <octaviorichter@gmail.com>
2019-06-17 22:04:57 -0300

    More package improvements
    - Create and add to load-path "lisp" folder
    - Define expand selection to quotes function and kbd
    - Fixes import of environment variables with exec-path-from-shell
    - Import golint function
    - Improve Go autocompletion with auto-complete-mode & gocode

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
 
 (package-initialize)
 
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
 ;; Editor UI
 (global-linum-mode t)      ; show line numbers
 (tool-bar-mode -1)         ; remove tool bar
@@ -54,12 +56,25 @@
 (global-set-key (kbd "C-x C-b") 'buffer-menu)
 (global-unset-key (kbd "<C-down-mouse-1>")) ; disable mouse buffer menu
 
+;;------------------------- Go -------------------------;;
+(exec-path-from-shell-copy-env "GOPATH")
 ;; Applies goimports (if executable available, otherwise fmt) before saving go code
 (with-eval-after-load 'go-mode
   (add-hook 'before-save-hook 'gofmt-before-save)
   (let ((goimports (executable-find "goimports")))
     (when goimports
       (setq gofmt-command goimports))))
+
+;; Import golint func
+(add-to-list 'load-path (concat (getenv "GOPATH")  "/src/golang.org/x/lint/misc/emacs"))
+(require 'golint)
+
+;; Go autocomplete
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+;;------------------------------------------------------;;
+
+(ac-config-default)
 
 ;; Origami-mode keybindings
 (global-set-key (kbd "C-c f") 'origami-recursively-toggle-node)
@@ -69,13 +84,35 @@
 ;; Flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+(eval-after-load "flycheck"
+  '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
+
+;; Expand selection to quotes function
+(defun tata-select-text-in-quote ()
+  "Select text between the nearest left and right delimiters.
+Delimiters here includes the following chars: \"<>(){}[]“”‘’‹›«»「」『』【】〖〗《》〈〉〔〕（）
+This command select between any bracket chars, not the inner text of a bracket."
+  (interactive)
+  (let (($skipChars
+         (if (boundp 'tata-brackets)
+             (concat "^\"" tata-brackets)
+           "^\"<>(){}[]“”‘’‹›«»「」『』【】〖〗《》〈〉〔〕（）"))
+        $pos)
+    (skip-chars-backward $skipChars)
+    (setq $pos (point))
+    (skip-chars-forward $skipChars)
+    (set-mark $pos)))
+(global-set-key (kbd "C-c \"") 'tata-select-text-in-quote)
+
 ;; Customize
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (flycheck sudo-edit origami go-mode dracula-theme))))
+ '(package-selected-packages
+   (quote
+	(exec-path-from-shell auto-complete flycheck-color-mode-line flycheck sudo-edit origami go-mode dracula-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
